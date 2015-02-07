60fa1362bfb7eda3327872586161889e623b591f
Frank Gehann <gehannf@gmail.com>
2015-02-07 23:21:39 +0100

    chg: entry view now also displays all associated tweets

--- views/entry/view.php
+++ views/entry/view.php
@@ -8,47 +8,74 @@
 
 use yii\helpers\Html;
 use yii\widgets\DetailView;
+use yii\grid\GridView;
+use yii\data\ActiveDataProvider;
+use app\models\Tweet;
 
 /* @var $this yii\web\View */
 /* @var $model app\models\Entry */
 
-$this->title = $model->id;
+$this->title = Yii::t('app', 'Entry') . " - " . $model->id;
 $this->params['breadcrumbs'][] = ['label' => Yii::t('app', 'Entries'), 'url' => ['index']];
 $this->params['breadcrumbs'][] = $this->title;
 ?>
-<div class="entry-view">
 
-    <h1><?= Yii::t('app', 'Entry') ?> - <?= Html::encode($this->title) ?></h1>
-
-    <p>
-    <?php
-        if(!Yii::$app->user->isGuest) 
-        {
-            echo Html::a(Yii::t('app', 'Update'), ['update', 'id' => $model->id], ['class' => 'btn btn-primary']);
-            echo "&nbsp;";
-            echo Html::a(Yii::t('app', 'Delete'), ['delete', 'id' => $model->id], [
-                'class' => 'btn btn-danger',
-                'data' => [
-                    'confirm' => Yii::t('app', 'Are you sure you want to delete this item?'),
-                    'method' => 'post',
+<div class="row">
+    <div class="col-lg-12">
+        <div class="entry-view">
+            <h1><?= Html::encode($this->title) ?></h1>
+            <p>
+            <?php
+                if(!Yii::$app->user->isGuest) 
+                {
+                    echo Html::a(Yii::t('app', 'Update'), ['update', 'id' => $model->id], ['class' => 'btn btn-primary']);
+                    echo "&nbsp;";
+                    echo Html::a(Yii::t('app', 'Delete'), ['delete', 'id' => $model->id], [
+                        'class' => 'btn btn-danger',
+                        'data' => [
+                            'confirm' => Yii::t('app', 'Are you sure you want to delete this item?'),
+                            'method' => 'post',
+                        ],
+                    ]);
+                }
+            ?>
+            </p>
+            <?= DetailView::widget([
+                'model' => $model,
+                'attributes' => [
+                    [
+                        'attribute' => 'contest_id',
+                        'value' => $model->contest->name
+                    ],
+                    'avg_rating',
+                    'min_rating',
+                    'max_rating',
+                    'votes',
                 ],
-            ]);
-        }
-    ?>
-    </p>
+            ]) ?>
+        </div>
+    </div>
+</div>
+<div class="row">
+    <div class="col-lg-12">
+        <h2><?= Yii::t('app', 'Tweets') ?></h2>
 
-    <?= DetailView::widget([
-        'model' => $model,
-        'attributes' => [
-            [
-                'attribute' => 'contest_id',
-                'value' => $model->contest->name
-            ],
-            'avg_rating',
-            'min_rating',
-            'max_rating',
-            'votes',
-        ],
-    ]) ?>
-
+        <? Pjax::begin(); ?>
+        <?= GridView::widget([
+                'dataProvider' => new ActiveDataProvider([
+                    'query' => Tweet::find(['entry_id' => $model->id]),
+                ]),
+                'columns' => [
+                    [
+                        'attribute' => 'user.screen_name',
+                        'value'=> function ($data) { 
+                            return $data->user->screen_name; 
+                        },
+                    ],     
+                    'created_at:datetime',
+                    'text'
+                ]
+        ]); ?>
+        <? Pjax::end(); ?>
+    </div>
 </div>
